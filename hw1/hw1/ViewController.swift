c662e04e9fff6d804a6caa2582f8b2c10173ffb3
Rustem <47086556+ironrustem@users.noreply.github.com>
2021-09-19 20:01:10 +0300

    [CREATED BUTTON]

--- hw1/hw1/ViewController.swift
+++ hw1/hw1/ViewController.swift
@@ -9,16 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    private lazy var branch1Label: UILabel = {
+    private lazy var branche1Label: UILabel = {
         let label = UILabel()
         label.frame = CGRect(x: 200, y: 200, width: 100, height: 100)
         label.text = "Branch1"
         return label
     }()
+    
+    private lazy var branche1Button: UIButton = {
+        let button = UIButton()
+        button.frame = CGRect(x: 300, y: 300, width: 100, height: 100)
+        button.setTitle("Branche1", for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(branch1Label)
+        addSubviews()
+    }
+    
+    func addSubviews() {
+        view.addSubview(branche1Label)
+        view.addSubview(branche1Button)
     }
 }
 
