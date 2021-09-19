a4467bc9053d0fcca0da56e6b88e03197771cb2d
Rustem <47086556+ironrustem@users.noreply.github.com>
2021-09-19 20:11:01 +0300

    [MADE CONFLICTS FOR BRANCH1]

--- hw1/hw1/ViewController.swift
+++ hw1/hw1/ViewController.swift
@@ -7,13 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
+    
+    private lazy var branche1Label: UILabel = {
+           let label = UILabel()
+           label.frame = CGRect(x: 100, y: 200, width: 100, height: 100)
+           label.text = "Branch1"
+           return label
+       }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        addSubviews()
     }
-
-
+    
+    func addSubviews() {
+        view.addSubview(branche1Label)
+    }
 }
 
