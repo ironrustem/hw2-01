a54c4926c7346402c503f8787ece0363396efda2
Rustem <47086556+ironrustem@users.noreply.github.com>
2021-09-19 19:57:43 +0300

    [CREATED LABEL]

--- hw1/hw1/ViewController.swift
+++ hw1/hw1/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var branch1Label: UILabel = {
+        let label = UILabel()
+        label.frame = CGRect(x: 200, y: 200, width: 100, height: 100)
+        label.text = "Branch1"
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(branch1Label)
     }
-
-
 }
 
